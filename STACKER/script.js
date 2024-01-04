ff9e53e10631ddb3f8e44347642abc1c7f2de628
Simone Dario <simone.dario10@gmail.com>
2024-01-04 16:38:18 +0100

    stacker end

--- STACKER/script.js
+++ STACKER/script.js
@@ -1,6 +1,12 @@
 // RECUPERO DALLA PAGINA HTML TUTTI GLI ELEMENTI DI INTERESSE
 
 const grid = document.querySelector('.grid');
+const stackBtn = document.querySelector('.stack');
+const scoreCounter = document.querySelector('.score-counter');
+const endGameScreen = document.querySelector('.end-game-screen')
+const endGameText = document.querySelector('.end-game-text')
+const playAgainButton = document.querySelector('.play-again')
+
 const gridMatrix = [
     [0,0,0,0,0,0],
     [0,0,0,0,0,0],
@@ -14,6 +20,10 @@ const gridMatrix = [
 ];
 let currentRowIndex = gridMatrix.length - 1;
 let barDirection = 'right';
+let barSize = 3;
+let isGameOver = false;
+let score = 0;
+let t;
 
 function draw(){
     grid.innerHTML = '';
@@ -73,11 +83,80 @@ function moveBar(){
     }
 }
 
+function checkLost(){
+    const currentRow = gridMatrix[currentRowIndex];
+    const prevRow = gridMatrix[currentRowIndex + 1];
+
+    if (!prevRow) return;
+    
+    for (let i=0; i<currentRow.length; i++){
+        if(currentRow[i] === 1 && prevRow[i] === 0){
+            currentRow[i] = 0;
+            barSize--;
+        }
+    }
+
+    if (barSize === 0){
+        isGameOver = true;
+        clearInterval(t);
+        endGame(false);
+    }
+}
+
+function checkWin()
+{
+    if (currentRowIndex === 0){
+        isGameOver = true;
+        clearInterval(t);
+        endGame(true);
+    }
+}
+
+function onStack (){
+    // check lost and win
+    checkLost();
+    checkWin();
+
+    updateScore();
+
+    if(isGameOver) return;
+
+    //cambio riga
+    currentRowIndex = currentRowIndex - 1;
+    barDirection = 'right';
+
+    for (let i=0; i < barSize; i++){
+        gridMatrix[currentRowIndex][i] = 1; 
+    }
+
+    draw();
+}
+
+function updateScore(){
+    score++;
+    scoreCounter.innerText = score.toString().padStart(5, '0'); //pad start scrive gli zeri fino a che le cifre sono 5
+
+}
+
+function endGame(isVictory){
+    if (isVictory){
+        endGameScreen.classList.add('win');
+        endGameText.innerHTML = 'YOU<br>WIN';
+    }
+    endGameScreen.classList.remove('hidden');
+}
+
+draw();
 
 function main(){
-    draw();
+
     moveBar();
     draw();
 }
 
-setInterval(main, 500);
+stackBtn.addEventListener('click', onStack);
+playAgainButton.addEventListener('click', function(){
+    location.reload();
+});
+
+t = setInterval(main, 500);
