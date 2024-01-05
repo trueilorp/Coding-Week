531fe947b3e7552cc0963ddab67f7222c27059aa
Simone Dario <simone.dario10@gmail.com>
2024-01-05 16:50:05 +0100

    Inizialization DUCKER

--- DUCKER/script.js
+++ DUCKER/script.js
@@ -8,155 +8,118 @@ const endGameText = document.querySelector('.end-game-text');
 const playAgainButton = document.querySelector('.play-again');
 
 const gridMatrix = [
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0]
+    ['','','','','','','','',''],
+    ['river', 'wood','wood','river', 'wood','river','river','river','river',],
+    ['river', 'river', 'river', 'wood', 'wood','river', 'wood', 'wood', 'river'],
+    ['','','','','','','','',''],
+    ['road', 'bus','road','road', 'road','car','road','road','road'],
+    ['road', 'road', 'road', 'car', 'road','road', 'road', 'road', 'bus'],
+    ['road', 'road', 'car', 'road', 'road','road', 'bus', 'road', 'road'],
+    ['','','','','','','','',''],
+    ['','','','','','','','','']
 ];
-let currentRowIndex = gridMatrix.length - 1;
-let barDirection = 'right';
-let barSize = 3;
-let isGameOver = false;
-let score = 0;
-let t;
 
-function draw(){
-    grid.innerHTML = '';
-    gridMatrix.forEach(function(rowContent, rowIndex){
-        rowContent.forEach(function(cellContent, cellIndex){
+const victoryRow = 0;
+const riverRows = [1,2];
+const roadRows = [4,5,6];
+const duckPosition = {y: 8, x: 4};
+let contentBeforeDuck = '';
+
+function applyCellStyle (cell, rowIndex, cellIndex){
+    const isRowEven = rowIndex % 2 === 0;
+                const isCellEven = cellIndex % 2 === 0;
+                if (isRowEven && isCellEven){
+                    cell.classList.add('cell-dark');
+                }else if (!isRowEven && !isCellEven){
+                    cell.classList.add('cell-dark');
+                }
+}
+function drawGrid(){
+    grid.innerHTML = ''; //pulisco i contenuti precedenti
+
+    gridMatrix.forEach(function(rowCells, rowIndex){
+        rowCells.forEach(function(cellContent, cellIndex){
             const cell = document.createElement('div');
             cell.classList.add('cell');
 
-            const isRowEven = rowIndex % 2 === 0;
-            const isCellEven = cellIndex % 2 === 0;
-
-            if ((isRowEven && isCellEven)||(!isRowEven && !isCellEven)){
-                cell.classList.add('cell-dark');
+            if(cellContent !== '') cell.classList.add(cellContent); //aggiungo ad ogni cella la sua classe di appartenenza, quindi se è river aggiungo la classe river
+            
+            //colore le righe in maniera appropiata
+            if(riverRows.includes(rowIndex)){
+                cell.classList.add('river');
+            }else if (roadRows.includes(rowIndex)){
+                cell.classList.add('road');
+            }else{
+                applyCellStyle(cell, rowIndex, cellIndex);                
             }
-
-            if(cellContent === 1){
-                cell.classList.add('bar');
-            }
-
+            
             grid.appendChild(cell);
         });
     });
 }
 
-function moveRight(row){
-    row.pop();
-    row.unshift(0);
+function placeDuck (){
+    //Prima di mettere la papera mi segno cosa cera
+    contentBeforeDuck = gridMatrix[duckPosition.y][duckPosition.x];
+
+    gridMatrix[duckPosition.y][duckPosition.x] = 'duck';
 }
 
-function moveLeft(row){
-    row.shift();
-    row.push(0);
-}
-
-function isRightEdge(row){
-    const lastElement = row[row.length - 1];
-    return lastElement === 1;
-}
-
-function isLeftEdge(row){
-    const firstElement = row[0];
-    return firstElement === 1;
-}
-
-function moveBar(){
-    const row = gridMatrix[currentRowIndex];
-    if (barDirection === 'right'){
-        moveRight(row);
-        if (isRightEdge(row)){
-            barDirection = 'left';
-        }
-    }else if (barDirection === 'left'){
-        moveLeft(row);
-        if (isLeftEdge(row)){
-            barDirection = 'right';
-        }
+function moveDuck(event){
+    //Prima di muovera la papera rimetto quello che c'era
+    gridMatrix[duckPosition.y][duckPosition.x] = contentBeforeDuck;
+    switch(event.key){
+        case 'ArrowUp':
+            if(duckPosition.y > 0) duckPosition.y--;
+            break;
+        case 'ArrowDown':
+            if(duckPosition.y < 8) duckPosition.y++;
+            break;
+        case 'ArrowLeft':
+            if(duckPosition.x > 0) duckPosition.x--;
+            break;
+        case 'ArrowRight':
+            if(duckPosition.x < 8) duckPosition.x++;
+            break;
     }
+    drawElements();
 }
 
-function checkLost(){
-    const currentRow = gridMatrix[currentRowIndex];
-    const prevRow = gridMatrix[currentRowIndex + 1];
-
-    if (!prevRow) return;
-    
-    for (let i=0; i<currentRow.length; i++){
-        if(currentRow[i] === 1 && prevRow[i] === 0){
-            currentRow[i] = 0;
-            barSize--;
-        }
-    }
-
-    if (barSize === 0){
-        isGameOver = true;
-        clearInterval(t);
-        endGame(false);
-    }
+function drawElements(){
+    placeDuck();
+    checkDuckPosition();
+    drawGrid();
 }
 
-function checkWin()
-{
-    if (currentRowIndex === 0){
-        isGameOver = true;
-        clearInterval(t);
-        endGame(true);
-    }
-}
-
-function onStack (){
-    // check lost and win
-    checkLost();
-    checkWin();
-
-    updateScore();
-
-    if(isGameOver) return;
-
-    //cambio riga
-    currentRowIndex = currentRowIndex - 1;
-    barDirection = 'right';
-
-    for (let i=0; i < barSize; i++){
-        gridMatrix[currentRowIndex][i] = 1; 
-    }
-
-    draw();
-}
-
-function updateScore(){
-    score++;
-    scoreCounter.innerText = score.toString().padStart(5, '0'); //pad start scrive gli zeri fino a che le cifre sono 5
-
-}
-
-function endGame(isVictory){
-    if (isVictory){
+function endGame(reason){
+    if(reason === 'duck-arrived'){
         endGameScreen.classList.add('win');
         endGameText.innerHTML = 'YOU<br>WIN';
     }
-    endGameScreen.classList.remove('hidden'); //se si perde viene rimosso l'hidden cosi viene mostrata la scritta game-over
+    document.removeEventListener('keyup', moveDuck); //si mette in attesa di un tasto schiacciato
+    gridMatrix[duckPosition.y][duckPosition.x] = reason;
+    endGameScreen.classList.remove('hidden');
 }
 
-draw();
-
-function main(){
-
-    moveBar();
-    draw();
+function checkDuckPosition(){
+    if(duckPosition.y === victoryRow) endGame('duck-arrived');
+    else if (contentBeforeDuck === 'river') endGame('duck-drowned');
+    else if (contentBeforeDuck === 'bus') endGame('duck-hit');
+    else if (contentBeforeDuck === 'car') endGame('duck-hit');
 }
- 
-stackBtn.addEventListener('click', onStack);
+
+
+/*********
+ * SVOLGIMENTO GIOCO
+ */
+
+drawElements();
+
+/*********
+ * EVENTI DI GIOCO
+ */
+
+document.addEventListener('keyup', moveDuck); //si mette in attesa di un tasto schiacciato
 playAgainButton.addEventListener('click', function(){
     location.reload();
 });
-
-t = setInterval(main, 600);
